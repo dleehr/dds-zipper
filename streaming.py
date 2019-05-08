fa5f631640f2b657bc440f390b05df84c55a377f
Dan Leehr <dan.leehr@duke.edu>
2019-05-08 15:14:52 -0400

    Implement actual chunked downloads using requests

--- streaming.py
+++ streaming.py
@@ -9,23 +9,26 @@ import zipstream
 urls = [
   ('https://raw.githubusercontent.com/Duke-GCB/bespin-api/master/Dockerfile', 'bespin-api/Dockerfile'),
   ('https://raw.githubusercontent.com/Duke-GCB/bespin-api/master/requirements.txt', 'bespin-api/requirements.txt'),
+  ('https://github.com/allanlei/python-zipstream/archive/master.zip', 'zipstream/master.zip'),
   ]
 
 def fetch(url):
-  print('fetching', url)
-  for _ in range(10):
-    yield b'bytes'
+  print('FETCH {}'.format(url))
+  r = requests.get(url, stream=True)
+  for chunk in r.iter_content(chunk_size=None):
+    print('RECEIVE {}'.format(len(chunk)))
+    yield chunk
 
 z = zipstream.ZipFile()
 
 for (url, filename) in urls:
+  print('calling write_iter with {}'.format(filename))
   z.write_iter(filename, fetch(url))
-  print('wrote', filename)
-
 
 # finally, write the zipfile
 with open('zipfile.zip', 'wb') as f:
   for data in z:
+    print('WRITE {}'.format(len(data)))
     f.write(data)
 
 # write_iter takes a name and a function that yields bytes from a file
