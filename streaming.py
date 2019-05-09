a84376417daeb65d4df4554284660b1fc579c806
Dan Leehr <dan.leehr@duke.edu>
2019-05-08 22:39:58 -0400

    Update flask route to take the project_id as the url path
    - fetches the project name
    - uses project name as attachemnt name
    - removes junk and urls list

--- streaming.py
+++ streaming.py
@@ -6,16 +6,6 @@ from ddsc.config import Config
 from flask import Flask, Response
 app = Flask(__name__)
 
-# project_id = 'ea4b60ed-b145-49ab-a41f-6ae122e06630' # bigger
-project_id = '20c1b14c-91c6-4a30-ab5e-aec4d632ee65'
-my_token = ''
-
-urls = [
-  ('http://hgdownload.cse.ucsc.edu/goldenpath/hg38/bigZips/hg38.fa.gz', 'bigZips/hg38.fa.gz'),
-  ('http://hgdownload.cse.ucsc.edu/goldenpath/hg38/bigZips/hg38.fa.masked.gz', 'bigZips/hg38.fa.masked.gz'),
-  ('http://hgdownload.cse.ucsc.edu/goldenpath/hg38/bigZips/hg38.fa.out.gz', 'bigZips/hg38.fa.out.gz'),
-  ]
-
 def make_config(token):
   c = Config()
   c.update_properties({Config.AUTH: token})
@@ -42,35 +32,41 @@ def get_dds_paths(client, project_id):
     ptf.add_paths_for_children_of_node(child)
   return ptf.paths # OrderedDict of path -> File
 
-def stream():
+def build_zip(client, project_id):
   z = zipstream.ZipFile()
-#   config = make_config(my_token)
-  client = Client()
   paths = get_dds_paths(client, project_id)
   for (filename, dds_file) in paths.items():
     print('write_iter {}'.format(filename))
     z.write_iter(filename, fetch(client, dds_file))
   return z
 
-@app.route("/zipfile.zip", methods=['GET'], endpoint='zipfile')
-def zipfile():
+def get_project_name(client, project_id):
+  project = client.get_project_by_id(project_id)
+  return project.name
+
+@app.route("/download-project/<project_id>.zip", methods=['GET'])
+def download(project_id):
+  client = Client() # This assumes it can authenticate
+  project_name = get_project_name(client, project_id)
   def generate():
-    z = stream()
+    z = build_zip(client, project_id)
     for chunk in z:
       print('WRITE {}'.format(len(chunk)))
       yield chunk
-
   response = Response(generate(), mimetype='application/zip')
-  response.headers['Content-Disposition'] = 'attachment; filename={}'.format('zipfile.zip')
+  response.headers['Content-Disposition'] = 'attachment; filename={}'.format('{}.zip'.format(project_name))
   print('RESPOND')
   return response
 
 def main():
-    z = stream()
-    with open('zipfile.zip', 'wb') as f:
-      for data in z:
-        print('WRITE {}'.format(len(data)))
-        f.write(data)
+  project_id = '20c1b14c-91c6-4a30-ab5e-aec4d632ee65'
+  client = Client()
+  project_name = get_project_name(client, project_id)
+  z = build_zip(client, project_id)
+  with open('{}.zip'.format(project_name), 'wb') as f:
+    for data in z:
+      print('WRITE {}'.format(len(data)))
+      f.write(data)
 
 if __name__=='__main__':
   main()
