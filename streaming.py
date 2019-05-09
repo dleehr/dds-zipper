68076cb638b67bc0d04b552aaff34114d0a1934c
Dan Leehr <dan.leehr@duke.edu>
2019-05-08 22:20:26 -0400

    Stub for make_config
    - would work with a DDS token, but those expire shortly
    - This may make more sense under gcb-web-auth, where we can refresh the token?

--- streaming.py
+++ streaming.py
@@ -1,18 +1,25 @@
 import requests
 import zipstream
 from ddsc.sdk.client import Client, PathToFiles
+from ddsc.config import Config
 
 from flask import Flask, Response
 app = Flask(__name__)
 
-project_name = 'pythonint_test'
+# project_id = 'ea4b60ed-b145-49ab-a41f-6ae122e06630' # bigger
 project_id = '20c1b14c-91c6-4a30-ab5e-aec4d632ee65'
+my_token = ''
 
 urls = [
   ('http://hgdownload.cse.ucsc.edu/goldenpath/hg38/bigZips/hg38.fa.gz', 'bigZips/hg38.fa.gz'),
   ('http://hgdownload.cse.ucsc.edu/goldenpath/hg38/bigZips/hg38.fa.masked.gz', 'bigZips/hg38.fa.masked.gz'),
   ('http://hgdownload.cse.ucsc.edu/goldenpath/hg38/bigZips/hg38.fa.out.gz', 'bigZips/hg38.fa.out.gz'),
   ]
+
+def make_config(token):
+  c = Config()
+  c.update_properties({Config.AUTH: token})
+  return c
 
 def get_url(client, dds_file):
   print('GETURL')
@@ -37,6 +44,7 @@ def get_dds_paths(client, project_id):
 
 def stream():
   z = zipstream.ZipFile()
+#   config = make_config(my_token)
   client = Client()
   paths = get_dds_paths(client, project_id)
   for (filename, dds_file) in paths.items():
